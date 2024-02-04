88f62eded5f8ff69a91fe72e7696497f69c2939c
Maeva Fortune <fortunemaeva2001@gmail.com>
2024-02-04 15:37:19 +0100

    Style slider images

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   chrome.runtime.sendMessage(
+    //Send message to the background script
     {
       action: "getNbImages",
     },
     function (response) {
+      //Get the answer of the background
       console.log(response);
     }
   );
