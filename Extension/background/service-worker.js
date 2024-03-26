817015c07b90aa91b54a74e37472149c73adcf83
Maeva Fortune <fortunemaeva2001@gmail.com>
2024-03-26 01:20:34 +0100

    Fix receiving audios

--- Extension/background/service-worker.js
+++ Extension/background/service-worker.js
@@ -1,52 +1,53 @@
 let imagesUrl = [];
 
+/**
+ * Start the script when the page is loaded
+ */
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (tab) {
     execScript(tab);
   } else {
-    alert("There are no active tabs");
+    console.log("There are no active tabs");
   }
 });
 
 /**
- * Execute a grabImages() function on a web page,
+ * Execute grabImages() function on a web page,
  * opened on specified tab and on all frames of this page
  * @param tab - A tab to execute script on
  */
 function execScript(tab) {
-  // Execute a function on a page of the current browser tab
-  // and process the result of execution
   chrome.scripting
     .executeScript({
       target: { tabId: tab.id, allFrames: true },
       func: grabImages,
     })
     .then((injectionResults) => {
-      // console.log(injectionResults);
       for (const frameResult of injectionResults) {
         const { frameId, result } = frameResult;
-        console.log(`Frame ${frameId} result:`, result);
         imagesUrl = result;
       }
     });
 }
 
+/**
+ * Get all the url of frames of a page
+ * @returns 
+ */
 function grabImages() {
   const images = document.querySelectorAll("img");
-  let frames = Array.from(images).map((image) => image.src);
-  // If script execution failed on the remote end
-  // and could not return results
+
+  let frames = Array.from(images).map((image) => image.src.replace(/'[^\x00-\x7F]'/gi, ''));
   if (!frames || !frames.length) {
-    alert("Could not retrieve images from specified page");
+    console.log("Could not retrieve images from this page");
     return;
   }
   return frames;
 }
 
-function getImagesUrl() {
-  console.log("show");
-}
-
+/**
+ * Handle requests from the popup
+ */
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "getNbImages") {
     sendResponse({ nb: imagesUrl.length });
--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,31 +1,54 @@
+var currentBlob = ""
+var pathVisualisation = ""
+var pathListen = ""
+
+/**
+ * Start when the DOM load
+ */
 document.addEventListener("DOMContentLoaded", function () {
+  // Get the number of images from the background script
   chrome.runtime.sendMessage(
-    //Send message to the background script
     {
       action: "getNbImages",
     },
     function (response) {
-      //Get the answer of the background
-      console.log(response.nb);
-      addElementsSlider(response.nb)
+      if(response.nb == undefined || response.nb == 0){
+        document.getElementById("loading-text").classList.remove("hidden");
+      }else{
+        document.getElementById("ct-slider").classList.remove("hidden");
+        console.log(response.nb);
+        addElementsSlider(response.nb)
+      }
+
     }
-  );
+  );  
+
+  // Set up buttons
+  const btnPlayVisualize = document.getElementById("visualize");
+  const btnPlayListen = document.getElementById('listen');
+  const btnSave = document.getElementById('save');
+  btnPlayVisualize.onclick = function() { playAudio(pathVisualisation) };
+  btnPlayListen.onclick = function() { playAudio(pathListen) };
+  btnSave.onclick = function() { saveAudio() };
 
 });
 
-// create as much buttons as the number of images in the page
+/**
+ * Create as much buttons as the number of elements
+ * @param {*} nbElements number of images in the page
+ */
 function addElementsSlider(nbElements) {
   
-  for (let index = 1; index <= nbElements; index++) {
+  for (let index = 0; index < nbElements; index++) {
     const button = document.createElement("button");
 
-    const indexButton = document.createTextNode(index+"");
+    const indexButton = document.createTextNode((index+1)+"");
 
     button.appendChild(indexButton);
 
     button.classList.add("item")
 
-    if(index == 1){
+    if(index == 0){
       button.classList.add("active")
     }
 
@@ -40,39 +63,40 @@ function addElementsSlider(nbElements) {
  
 }
 
-function getURL(indexImage) {
+/**
+ * Process the image and set up the sound
+ * @param {*} indexImage of the selected image
+ */
+function processUrl(indexImage) {
   chrome.runtime.sendMessage(
-    //Send message to the background script
     {
       getURL: indexImage,
     },
     async function (message) {
       //Get the answer of the background
-      fetch("http://127.0.0.1:5000/post-sound/", {
-        method: "POST",
-        body: JSON.stringify({
-          url: message.url
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      })
-      .then((response) => response.json())
-      .then((json) => encoded(json));
-      
-      function encoded(json){
-        console.log(json);
-        console.log(json.encodedSound);
-      
-      }
-      /* var snd = new Audio("data:audio/x-wav;base64"+soundBase64);
-      snd.play(); */
+      url =  message.url.replace(/'[^\x00-\x7F]'/gi, '');
+      paths = await postImage(url)         
+      pathVisualisation = paths.pathVisu
+      pathListen = paths.pathListen
       
     }
   );
 }
 
+/**
+ * Handle action of the selected image
+ * @param {*} button 
+ */
 function handleActiveButton(button){
+  currentBlob = ""
+  pathVisualisation = ""
+  pathListen = ""
+
+  const optionsAudio = document.getElementById("ct-audio")
+  if(!optionsAudio.classList.contains("hidden")){
+    optionsAudio.classList.add("hidden");
+  }
+
   activeButton = document.getElementsByClassName("active")[0]
   activeButton.classList.remove("active")
 
@@ -80,6 +104,79 @@ function handleActiveButton(button){
 
   index = button.getAttribute('data-idBtt')
 
-  getURL(index)
+  options = document.getElementById("options")
+  if( options != null || options.classList.contains("hidden")){
+    options.style.visibility = "visible"
+  }
+
+  processUrl(index)
 }
 
+/**
+ * Call flask server to process the image and save it on the serv
+ * @param {*} url of the image
+ * @returns the name of the wav file saved
+ */
+async function postImage(url){
+
+ return fetch("http://127.0.0.1:5000/post-sound/", {
+    method: "POST",
+    body: JSON.stringify({
+      url: url
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then((response) => response.json())
+  .then((json) => json);
+  
+}
+
+/**
+ * Call flask server to get the audio file and transform it as a blob data
+ * Set up the received audio file into the audio element
+ * @param {*} path name of the wav file
+ */
+async function getSound(path){
+  fetch("http://127.0.0.1:5000/get-sound/"+path, {
+    method: "GET",
+  })
+  .then((response) => response.blob())
+  .then((blob) => {
+    currentBlob = new Blob([blob], { type: 'audio/wav' });
+    soundUrl = URL.createObjectURL(currentBlob);
+    const audioElement = document.getElementById("audioPlayer");
+    audioElement.setAttribute("src", soundUrl);
+  })
+  .catch(error => {
+    console.error('Error fetching the audio file:', error);
+  });
+}
+
+/**
+ * Start the audio
+ */
+async function playAudio(path){
+    currentBlob = ""
+    await getSound(path);
+    console.log(path);
+    const options = document.getElementById("ct-audio")
+    if(options.classList.contains("hidden")){
+      options.classList.remove("hidden");
+    }
+    const audioElement = document.getElementById("audioPlayer");
+    audioElement.addEventListener("loadedmetadata", function () {
+    });
+
+}
+
+/**
+ * Save the audio in the computer
+ * @param {*} name 
+ */
+function saveAudio(name){
+  if(currentBlob != ""){
+    saveAs(currentBlob, name+'.wav');
+  }
+}
