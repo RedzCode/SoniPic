6c90e441f2ee19f534e2ccc2f2e56f1672ba2230
Maeva Fortune <fortunemaeva2001@gmail.com>
2024-04-01 19:29:44 +0200

    Add call to server

--- Website/script/sonification.js
+++ Website/script/sonification.js
@@ -1,22 +1,40 @@
 
+async function sonifyImage(){
+    blobImg = document.getElementById("picture-input").files[0];
+    var formData = new FormData();
+    // Append the file to FormData
+    formData.append('image', blobImg);
+/*     var reader = new FileReader
+    reader.readAsDataURL(blobImg)
+    var data = reader.result;
+    data = data.split(",").pop(); */
+    paths = await postImage(formData)
+}
 
-async function postImage(url){
+async function loadAudios(){
+    img = document.getElementById("uploaded")
+    await postImage(img)
+}
+
+async function postImage(data){
 
 return fetch("http://127.0.0.1:5000/post-sound/", {
     method: "POST",
-    body: JSON.stringify({
-        url: url
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
+    body: data,
     })
     .then((response) => response.json())
-    .then((json) => json.path);
+    .then(async (json) => {    
+        paths = json.path
+        pathVisualisation = paths.pathVisu
+        pathListen = paths.pathListen
+        await getSound(pathVisualisation, "audio-visu")
+        await getSound(pathListen, "audio-listen")
+    }
+);
     
 }
 
-async function getSound(path){
+async function getSound(path, idPlayer){
     fetch("http://127.0.0.1:5000/get-sound/"+path, {
     method: "GET",
     })
@@ -25,7 +43,7 @@ async function getSound(path){
     // Create a new Blob object with the fetched data
     blob = new Blob([blob], { type: 'audio/wav' });
     soundUrl = URL.createObjectURL(blob);
-    const audioElement = document.getElementById("audioPlayer");
+    const audioElement = document.getElementById(idPlayer);
     audioElement.setAttribute("src", soundUrl);
     })
     .catch(error => {
