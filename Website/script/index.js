c4e871f219b221026f98913459b3dec2762d5f1f
Maeva Fortune <fortunemaeva2001@gmail.com>
2024-04-04 22:21:45 +0200

    Reset audio players

--- Website/script/index.js
+++ Website/script/index.js
@@ -4,5 +4,9 @@
 function displayPicture(event) {
     var image = document.getElementById('uploaded');
 	image.src = URL.createObjectURL(event.target.files[0]);
+    const audioElement1 = document.getElementById("audio-abs");
+    const audioElement2 = document.getElementById("audio-cr");
+    audioElement1.setAttribute("src", "");
+    audioElement2 .setAttribute("src", "");
 }
 
